cff00d991284274927697301533dd087bf02479a
Inthetouch <nikita_eremeev@mail.ru>
2025-06-29 21:18:17 +0300

    add lint

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-import { parse, genDiff } from './parser.js';
+import { Command } from 'commander'
+import { parse, genDiff } from './parser.js'
 
-const program = new Command();
+const program = new Command()
 
 program
   .version('0.0.1')
@@ -11,8 +11,8 @@ program
   .argument('<filepath2>')
   .option('-f, --format <type>', 'output format')
   .action((filepath1, filepath2) => {
-    const diff = genDiff(parse(filepath1), parse(filepath2));
-    console.log(diff);
+    const diff = genDiff(parse(filepath1), parse(filepath2))
+    console.log(diff)
   })
 
-program.parse();
+program.parse()
--- src/parser.js
+++ src/parser.js
@@ -1,34 +1,36 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'fs'
+import path from 'path'
 
 export function parse(filepath) {
+  const absolutePath = path.resolve(process.cwd(), filepath)
+  const content = fs.readFileSync(absolutePath, 'utf-8')
 
-  const absolutePath = path.resolve(process.cwd(), filepath);
-  const content = fs.readFileSync(absolutePath, 'utf-8');
-  
-  return JSON.parse(content);
+  return JSON.parse(content)
 }
 
 export function genDiff(objec1, objec2) {
-  const keys1 = Object.keys(objec1);
-  const keys2 = Object.keys(objec2);
+  const keys1 = Object.keys(objec1)
+  const keys2 = Object.keys(objec2)
 
   const result = {}
-  const allKeys = new Set([...keys1, ...keys2]);
+  const allKeys = new Set([...keys1, ...keys2])
 
   for (const key of allKeys) {
     if (key in objec1 && key in objec2) {
       if (objec1[key] === objec2[key]) {
-        result[key] = objec1[key];
-      } else {
-        result[`- ${key}`] = objec1[key];
-        result[`+ ${key}`] = objec2[key];
+        result[key] = objec1[key]
       }
-    } else if (key in objec1) {
-      result[`- ${key}`] = objec1[key];
-    } else if (key in objec2) {
-      result[`+ ${key}`] = objec2[key];
+      else {
+        result[`- ${key}`] = objec1[key]
+        result[`+ ${key}`] = objec2[key]
+      }
+    }
+    else if (key in objec1) {
+      result[`- ${key}`] = objec1[key]
+    }
+    else if (key in objec2) {
+      result[`+ ${key}`] = objec2[key]
     }
   }
-  return result;
+  return result
 }
