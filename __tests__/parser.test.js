a560ea97b1247468a3f6824df80b8d7827480450
Inthetouch <nikita_eremeev@mail.ru>
2025-07-02 19:19:49 +0300

    delete txt and add object in test

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -9,11 +9,18 @@ const __dirname = dirname(__filename)
 
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
-const expectResult = fs.readFileSync(getFixturePath('test_result.txt'), 'utf-8');
+const expectResult = {
+  host: 'hexlet.io',
+  '- timeout': 50,
+  '+ timeout': 20,
+  '- proxy': '123.234.53.22',
+  '- follow': false,
+  '+ verbose': true,
+};
 
 describe('Сравнение двух файлов', () => {
   test.each([
-    {file1: 'file.json', file2: 'file2.json', format: 'JSON'},
+    {file1: 'file1.json', file2: 'file2.json', format: 'JSON'},
     {file1: 'file1.yml', file2: 'file2.yaml', format: 'YAML'},
   ])('Проверка работы функции сравнения $format файлов', ({file1, file2}) => {
     const filepath1 = getFixturePath(file1)
--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,22 @@
 import fs from 'fs'
 import path from 'path'
+import yaml from 'js-yaml'
+
+const getFileFormat = (filepath) => path.extname(filepath).slice(1)
 
 export function parse(filepath) {
   const absolutePath = path.resolve(process.cwd(), filepath)
   const content = fs.readFileSync(absolutePath, 'utf-8')
+  const fileFormat = getFileFormat(absolutePath);
 
-  return JSON.parse(content)
+  if (fileFormat === 'json') {
+    return JSON.parse(content)
+  } else if (fileFormat === 'yml' || fileFormat === 'yaml') {
+    return yaml.load(content)
+  } else {
+    throw new Error(`Unsupported file format: ${fileFormat}`)
+  }
+  
 }
 
 export function genDiff(objec1, objec2) {
