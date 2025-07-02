a1df7d28cec645bd980d8b5ced268efaea7ede01
Inthetouch <nikita_eremeev@mail.ru>
2025-07-02 22:28:52 +0300

    linting

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -8,19 +8,19 @@ const __dirname = dirname(__filename)
 const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
 const expectResult = {
-  host: 'hexlet.io',
+  'host': 'hexlet.io',
   '- timeout': 50,
   '+ timeout': 20,
   '- proxy': '123.234.53.22',
   '- follow': false,
   '+ verbose': true,
-};
+}
 
 describe('Сравнение двух файлов', () => {
   test.each([
-    {file1: 'file1.json', file2: 'file2.json', format: 'JSON'},
-    {file1: 'file1.yml', file2: 'file2.yaml', format: 'YAML'},
-  ])('Проверка работы функции сравнения $format файлов', ({file1, file2}) => {
+    { file1: 'file1.json', file2: 'file2.json', format: 'JSON' },
+    { file1: 'file1.yml', file2: 'file2.yaml', format: 'YAML' },
+  ])('Проверка работы функции сравнения $format файлов', ({ file1, file2 }) => {
     const filepath1 = getFixturePath(file1)
     const filepath2 = getFixturePath(file2)
     expect(genDiff(parse(filepath1), parse(filepath2))).toEqual(expectResult)
--- src/parser.js
+++ src/parser.js
@@ -2,21 +2,22 @@ import fs from 'fs'
 import path from 'path'
 import yaml from 'js-yaml'
 
-const getFileFormat = (filepath) => path.extname(filepath).slice(1)
+const getFileFormat = filepath => path.extname(filepath).slice(1)
 
 export function parse(filepath) {
   const absolutePath = path.resolve(process.cwd(), filepath)
   const content = fs.readFileSync(absolutePath, 'utf-8')
-  const fileFormat = getFileFormat(absolutePath);
+  const fileFormat = getFileFormat(absolutePath)
 
   if (fileFormat === 'json') {
     return JSON.parse(content)
-  } else if (fileFormat === 'yml' || fileFormat === 'yaml') {
+  }
+  else if (fileFormat === 'yml' || fileFormat === 'yaml') {
     return yaml.load(content)
-  } else {
+  }
+  else {
     throw new Error(`Unsupported file format: ${fileFormat}`)
   }
-  
 }
 
 export function genDiff(objec1, objec2) {
