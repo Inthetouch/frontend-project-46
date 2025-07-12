785c14001f719b087ceeededd80dd09704ec650c
Inthetouch <nikita_eremeev@mail.ru>
2025-07-12 18:43:45 +0300

    fix gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format [type]', 'output format (default: "stylish")')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2)
     console.log(diff)
