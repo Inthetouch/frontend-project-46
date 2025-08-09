ca9aaed7934189c1af0a77227b7a31340c14a166
Inthetouch <nikita_eremeev@mail.ru>
2025-08-09 14:53:26 +0300

    fix export

--- src/difference.js
+++ src/difference.js
@@ -2,7 +2,7 @@ import { parseFile } from '../src/parser.js'
 import { buildAnser } from '../src/builder.js'
 import { format } from '../src/formatters/index.js'
 
-export function genDiff(file1, file2, formatName = 'stylish') {
+export default function genDiff(file1, file2, formatName = 'stylish') {
   const data1 = parseFile(file1)
   const data2 = parseFile(file2)
   const diff = buildAnser(data1, data2)
